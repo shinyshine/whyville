b9b710b53f72ad753aeb85972e950be92654011f
tongx <ly012599@qq.com>
2016-05-26 09:57:46 +0800

    two more pages in finance

--- app/js/controllers/finance/finance.js
+++ app/js/controllers/finance/finance.js
@@ -34,4 +34,78 @@ angular.module('homeApp.finance', ['ngRoute', 'homeApp.financeService'])
 				templateUrl: 'views/finance/applicate.html',
 				controller: 'payForApp'
 			})
+			.when('/daily', {
+				templateUrl: 'views/finance/daily.html',
+				controller: 'daily'
+			})
+			.when('/account', {
+				templateUrl: 'views/finance/account.html',
+				controller: 'account'
+			})
+	})
+
+	//财务日报
+	.controller('daily', function($scope) {
+		
+	})
+	//账户余额
+	.controller('account', function($scope, fetchAccounts, modifyAccount) {
+		$scope.options = {
+			"method": [{
+				"id": 0,
+				"name": '现金'
+			},{
+				"id": 1,
+				"name": '刷卡'
+			}]
+		}
+
+		$scope.filter = {
+			"method": {
+				"id": 0,
+				"name": '现金'
+			}
+		}
+		$scope.account = {
+			"total": '25555000',
+			"list": [{
+				"sec_type": {
+					"id": 1,
+					"name": '珠江新城校区'
+				},
+				"remain": '44444444',
+				"isEditing": 0
+			},{
+				"sec_type": {
+					"id": 2,
+					"name":'龙口西校区'
+				},
+				"remain": '66666',
+				"isEditing": 0
+			}]
+		}
+
+		fetchAccounts($scope.filter, function(result) {
+			console.log(result);
+		})
+
+		$scope.modify = function(index) {
+			var status = $scope.account[index].isEditing;
+			if(status) {	
+				modifyAccount($scope.account[index], function(result) {
+					if(result.status) {
+						alert('修改成功');
+					}
+				})
+				//$http 
+				console.log(data);
+			}
+			$scope.account[index].isEditing = !status;
+		}
+
+		$scope.sendFilter = function() {
+			fetchAccounts($scope.filter, function(result) {
+				console.log(result);
+			})
+		}
 	})
--- app/js/controllers/finance/service.js
+++ app/js/controllers/finance/service.js
@@ -11,30 +11,10 @@ angular.module('homeApp.financeService', [])
 			"fetchAppById": server + 'get_application_information',
 			"payForApp": server + 'accept_application',
 			"modPay": server + 'change_cost',
+			"modifyAccount": server + ''
 		}
 	})
 	.factory('fetchPayList', function(financeAPI) {
-		// var fetchData = function(filter) {
-		// 	return {
-		// 		"total": {
-		// 			"number": '50',    //一共多少项记录
-		// 			"cost": '50000000000000' //一共多少钱
-		// 		},
-		// 		"payList": [{
-		// 			"pay_id": '1',
-		// 			"school": '珠江新城校区',
-		// 			"pay_date": '2016-06-06',
-		// 			"pay_type": '管理费用',
-		// 			"pay_sec_type": '水电费',
-		// 			"pay_method": '现金',
-		// 			"pay_to": '珠江新城',
-		// 			"cost": '5000',
-		// 			"app_state": 0, //是否申请
-		// 			"abstraction": '摘要啊'
-		// 		}]
-		// 	}
-		// }
-
 		return function(data, callBack) {
 			getData(financeAPI.fetchPayList, callBack, data);
 		}
@@ -74,35 +54,6 @@ angular.module('homeApp.financeService', [])
 		}
 	})
 	.factory('fetchPayById', function(financeAPI) {
-		// var fetchData = function(pay_id) {
-		// 	return {
-		// 		"pay_id": 1,
-		// 		"school": {
-		// 			"id": 1,
-		// 			"name": '大学城校区'
-		// 		},
-		// 		"pay_date": '2016-06-03',
-		// 		"pay_type": {
-		// 			"id": '1',
-		// 			"name": '管理费用'
-		// 		},
-		// 		"pay_sec_type": {
-		// 			"id": '2',
-		// 			"name": '水电费'
-		// 		},
-		// 		"pay_method": {
-		// 			"id": '0',
-		// 			"name": '现金'
-		// 		},
-		// 		"pay_to": {
-		// 			"id": '2',
-		// 			"name": '珠江新城'
-		// 		},
-		// 		"cost": '5000',
-		// 		"app_state": 0, //是否申请
-		// 		"abstraction": '摘要啊'
-		// 	}
-		// }
 		return function(data, callBack) {
 			getData(financeAPI.modPay, callBack, data);
 		}
@@ -114,25 +65,6 @@ angular.module('homeApp.financeService', [])
 	})
 
 	.factory('fetchIncomeList', function(financeAPI) {
-		// var fetchData = function() {
-		// 	return {
-		// 		"total": {
-		// 			"number": '50',    //一共多少项记录
-		// 			"sum": '50000000000000' //一共多少钱
-		// 		},
-		// 		"incomeList": [{
-		// 			"in_id": 1, 
-		// 			"school": '大学城校区',
-		// 			"date": '2016-06-06',
-		// 			"in_type": '书费',
-		// 			"sum": '5000',
-		// 			"pay_method": '现金',
-		// 			"pay_to": '珠江新城校区',
-		// 			"abstraction": '摘要啊',
-		// 			"charge_name": '经手人姓名'
-		// 		}]
-		// 	}
-		// }
 		return function(data, callBack) {
 			getData(financeAPI.fetchIncomeList, callBack, data);
 		}
@@ -207,24 +139,6 @@ angular.module('homeApp.financeService', [])
 		}
 	})
 	.factory('fetchAppList', function(financeAPI) {
-		// var fetchData = function() {
-		// 	return {
-		// 		"total": {
-		// 			"number": 30,
-		// 			"cost": 60000000000000000
-		// 		},
-		// 		"appList": [{
-		// 			"app_id": 1,
-		// 			"school": '大学城校区',
-		// 			"emp_name": '罗半仙',
-		// 			"app_title": '购书',
-		// 			"date": '2016-06-06',
-		// 			"pay_method": '现金', //这一项和下面那一项，如果是还没有付款的，能不能返回空给我。。假设你能
-		// 			"pay_to": '天河校区'
-		// 		}]
-		// 	}
-		// }
-
 		return function(data, callBack) {
 			getData(financeAPI.fetchAppList, callBack, data);
 		}
@@ -238,24 +152,13 @@ angular.module('homeApp.financeService', [])
 
 	//获取某申请表的信息
 	.factory('fetchAppById', function(financeAPI) {
-		// var fetchData = function(app_id) {
-		// 	return {
-		// 		"app_id": 2222,
-		// 		"app_date": '2016-06-03',
-		// 		"school": '珠江新城校区',
-		// 		"emp_name": '罗半仙',
-		// 		"app_title": '购置办公用品',
-		// 		"content": '办公桌三张',
-		// 		"per_price": '500',
-		// 		"number": '20',
-		// 		"total_price": '10000',
-		// 		"accept_date": '2016-06-06', //审批日期
-		// 		"accept_name": '罗总经理', //审批人姓名
-		// 		"opinion": '让你买一次哈哈哈' //审批人意见
-		// 	}
-		// }
-
 		return function(data, callBack) {
 			getData(financeAPI.fetchAppById, callBack, data);
+		}
+	})
+
+	.factory('modifyAccount', function(financeAPI) {
+		return function(data, callBack) {
+			postData(financeAPI.modifyAccount, data, callBack);
 		}
 	})
